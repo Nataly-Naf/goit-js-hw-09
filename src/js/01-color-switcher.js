3cb9374bf06b866fb1728e038fab9b544a3f4567
Nataly-Naf <nat.spivak84@gmail.com>
2023-08-15 12:03:37 +0100

    1st task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,14 @@
 
+import { changeColorBody, onStartBtnClick, onStopBtnClick } from './handlers';
+import { refs } from './refs';
+
+        
+refs.startBtn.addEventListener('click', onStartBtnClick )
+
+
+refs.stopBtn.addEventListener('click', onStopBtnClick)
+
+
+
+
+
